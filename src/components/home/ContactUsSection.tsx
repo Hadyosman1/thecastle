b1b189f6958ed0789f3f3d1227e4909757b3bb8b
Hady Osman <hady.osman.dev@gmail.com>
2025-07-31 01:52:27 +0300

    Edit the contact-us section background images

--- src/components/home/ContactUsSection.tsx
+++ src/components/home/ContactUsSection.tsx
@@ -1,13 +1,31 @@
-import React from "react";
 import CTAButton from "../CTAButton";
+import Image from "next/image";
+import watchCover from "@/../public/watch_cover.webp";
+import watchPic from "@/../public/watch-1.webp";
 
 export default function ContactUsSection() {
   return (
     <section
       id="contact-us"
-      className="watch-cover-bg flex flex-col items-center justify-center"
+      className="flex min-h-[calc(100vh-var(--header-height))] flex-col items-center justify-center"
     >
-      <div className="bg-primary/90 text-center z-[2] my-12 flex w-full flex-col items-center gap-4 py-12">
+      <Image
+        src={watchCover}
+        alt="cover"
+        fill
+        className="hidden object-cover md:block"
+        unoptimized
+        quality={100}
+      />
+      <Image
+        src={watchPic}
+        alt="cover"
+        fill
+        className="object-cover md:hidden"
+        unoptimized
+        quality={100}
+      />
+      <div className="bg-primary/90 z-[2] my-12 flex w-full flex-col items-center gap-4 py-12 text-center">
         <h2 className="text-lg font-bold text-white md:text-2xl lg:text-4xl">
           تواصل معنا الآن
         </h2>
